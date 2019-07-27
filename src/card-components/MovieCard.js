0e13f9d4e7e2315deabffdde28ca49312e1f49a8
Tyler Hunt <tyhunt04@gmail.com>
2019-07-27 19:13:28 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -42,7 +42,7 @@ export default class MovieCard extends Component {
 
 MovieCard.defaultProps {
   title: 'Unknown',
-  IMDBRating: null;
-  genres: ['No Genre(s) Found']
+  IMDBRating: null',
+  genres: ['No Genre(s) Found'],
   poster: 'default'
 }
